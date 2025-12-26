ae5ada685098a2b7c63b9bc78317fea36b8fab70
Naveen Choudhary <naveenchoudhary00001@gmail.com>
2025-12-26 21:32:52 +0530

    amazon interview question part-1

--- SQL_Ankit_bansal_udemy/q6_Amazon_Interview_Question.sql
+++ SQL_Ankit_bansal_udemy/q6_Amazon_Interview_Question.sql
@@ -5,4 +5,11 @@ use ankit_bansal_udemy;
 -----------------Amazon Interview Question-------------------------------
 -----------------Amazon Interview Question-------------------------------
 
-
+/*
+Write a query to provide the date for nth occurrence of sunday in future from given date.
+datepart
+sunday-1
+monday-2
+friday-6
+saturday-7
+*/
