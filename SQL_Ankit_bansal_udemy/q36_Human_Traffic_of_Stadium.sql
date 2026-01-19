f2b32b437deaf0a5daffac323cbbddc7b6c5b606
Naveen Choudhary <naveenchoudhary00001@gmail.com>
2026-01-19 09:55:17 +0530

    q36-unsolved

--- SQL_Ankit_bansal_udemy/q36_Human_Traffic_of_Stadium.sql
+++ SQL_Ankit_bansal_udemy/q36_Human_Traffic_of_Stadium.sql
@@ -38,15 +38,62 @@ select * from stadium;
 
 with cte1 as (
 	select id, visit_date, no_of_people, 
-	sum(case when no_of_people >= 100 then 1 else 0 end) over(order by visit_date) as sum_flag
+	case when no_of_people >= 100 then 0 else 1 end as flag
 	from stadium
 ), cte2 as (
-	select *,
-	row_number() over(partition by sum_flag order by visit_date) as rn
+	select *, 
+	count(*) over(order by visit_date) as x
 	from cte1
 )
 	select * 
+	from cte2;
+
+
+
+
+
+with cte1 as (
+	select id, visit_date, no_of_people, 
+	case when no_of_people >= 100 then 0 else 1 end as flag
+	from stadium
+), cte2 as (
+	select *, 
+	row_number() over(order by visit_date) as rn_1 ,
+	row_number() over(partition by flag order by visit_date) as rn_2 
+	from cte1
+), cte3 as (
+	select *,
+	rn_1-rn_2 as new_grp
 	from cte2
+), cte4 as (
+	select visit_date, no_of_people, count(1) over(partition by new_grp) as cnt
+	from cte3
+) 
+	select *
+	from cte4
+	where cnt >= 3
+
+
+
+with cte_1 as (
+	select *,
+	lag(visit_date, 1,visit_date) over(order by visit_date) as visit_date_lag
+	from stadium
+	where no_of_people >= 100
+), cte_2 as (
+	select *,
+	case when datediff(day, visit_date_lag, visit_date) = 1 then 0 else 1 end as flag
+	from cte_1
+), cte_3 as (
+	select *, sum(flag) over(order by visit_date) sum_flag
+	from cte_2
+), cte_4 as (
+	select *, count(*) over(partition by sum_flag) as cnt
+	from cte_3
+)
+	select visit_date, no_of_people
+	from cte_4
+	where cnt >= 3;
 
 
 
@@ -60,10 +107,32 @@ with cte1 as (
 
 
 
+with cte_1 as (
+	select *,
+	lag(visit_date, 1,visit_date) over(order by visit_date) as visit_date_lag
+	from stadium
+	where no_of_people >= 100
+), cte_2 as (
+	select *,
+	case when datediff(day, visit_date_lag, visit_date) = 1 then 0 else 1 end as flag
+	from cte_1
+), cte_3 as (
+	select *, count(*) over(order by visit_date) as cnt
+	from cte_2
+), cte_4 as (
+	select *
+	from cte_3
+)
+	select *
+	from cte_4;
+	
 
 
 
 
+select *, sum(no_of_people) 
+over(order by visit_date) as sum_no_of_people from stadium
+where no_of_people >= 100
 
 
 
